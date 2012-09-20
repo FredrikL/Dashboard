7f5703b5246cc0bb04f8d627ac311c63430a0df8
Fredrik Leijon <fredrik.leijon@gmail.com>
2012-09-20 20:51:21 +0200

    initial commit

---
+++ Dashboard.Dev/DashboardModule.cs
@@ -0,0 +1,55 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace Dashboard.Dev
+{
+    public class DashboardModule :NancyModule
+    {
+        public DashboardModule()
+        {
+            Get["/"] = x =>
+                {
+                    var model = new Model();
+                    var t = new Trains();
+                    model.Trains =  t.GetTrains();
+                    return View["index", model];
+                };
+        }
+    }
+
+    public class Model
+    {
+        public IEnumerable<Trains.Deps> Trains { get; set; }
+    }
+
+    public class Trains
+    {
+        public class Deps
+        {
+            public string Time { get; set; }
+            public string EstTime { get; set; }
+            public string Destination { get; set; }
+
+        }
+
+        public IEnumerable<Deps> GetTrains()
+        {
+            var wc = new WebClient();
+            string data = wc.DownloadString("http://www5.trafikverket.se/taginfo/WapPages/StationShow.aspx?JF=-1&station=74,Åk&arrivals=0");
+            var r = new Regex(">[\\W]+(?<TIME>.+?) till (?<DEST>.+?)<br>");
+            
+            MatchCollection mc = r.Matches(data);
+            foreach (Match m in mc)
+            {
+                var t = m.Groups["TIME"].Captures[0].ToString();
+                var d = m.Groups["DEST"].Captures[0].ToString();
+                yield return new Deps { Destination = d, Time = t};
+            }
+        }
+    }
+}
---
+++ Dashboard/Program.cs
@@ -0,0 +1,43 @@
+﻿using Nancy;
+using Nancy.Hosting.Self;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dashboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nancyHost = new NancyHost(new Uri("http://localhost:8888/nancy/"), new Uri("http://127.0.0.1:8888/nancy/"));
+            nancyHost.Start();
+
+            Console.WriteLine("Nancy now listening - navigating to http://localhost:8888/nancy/. Press enter to stop");
+           
+            Console.ReadKey();
+
+            nancyHost.Stop();
+
+            Console.WriteLine("Stopped. Good bye!");
+        }
+    }
+
+    public class TestModule : NancyModule
+    {
+        public TestModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["staticview", Request.Url];
+            };
+
+            Get["/testing"] = parameters =>
+            {
+                return View["staticview", Request.Url];
+            };
+        }
+    }
+}
